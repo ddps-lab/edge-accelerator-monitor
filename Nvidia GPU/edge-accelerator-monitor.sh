595a8d885e27332d7dd8d2c0c603e392e6123da3
jungae18 <46885199+jungae18@users.noreply.github.com>
2021-11-23 11:01:35 +0900

    Delete Nvidia GPU directory

--- Nvidia GPU/edge-accelerator-monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nvidia-smi --query-gpu=driver_version,gpu_name --format=csv,noheader >> ./project/data.txt
-kubectl label node $(cat host_hostname) gpu.model=$(head -n 1 /project/data.txt | awk -F ',' '{print $1}') gpu.driver=$(head -n 1 /project/data.txt | awk -F ' ' '{print $2$3}') gpu.resource=nvidia-smi
